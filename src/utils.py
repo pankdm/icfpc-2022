3a4826f1315748e4ff0125cf7dc3f2f831ffc2ac
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2022-09-03 09:12:25 -0700

    Downloaded 26-30

--- src/utils.py
+++ src/utils.py
@@ -4,26 +4,35 @@ from PIL import Image
 import numpy as np
 import sys
 
+
+def download_file(from_url, to_filename):
+    resp = requests.get(from_url)
+    if resp.status_code == 200:
+        with open(to_filename, 'wb') as f:
+            f.write(resp.content)
+        print(f"File {to_filename} written")
+    else:
+        print(f"URL error {resp.status_code}: ${from_url}")
+
+
 def download_pic(n):
-    path=f"problems/{n}.png"
+    problems_dir = "../problems"
+    path = f"{problems_dir}/{n}.png"
     if not os.path.exists(path):
-        resp=requests.get(f"https://cdn.robovinci.xyz/imageframes/{n}.png")
-        if resp.status_code==200:  
-            with open(path, 'wb') as f:
-                f.write(resp.content)
-            print(f"File {n} written")
-        else:
-            print(f"URL error {resp.status_code}: https://cdn.robovinci.xyz/imageframes/{n}.png")
+        download_file(from_url=f"https://cdn.robovinci.xyz/imageframes/{n}.png",
+                      to_filename=f"{problems_dir}/{n}.png")
+        download_file(from_url=f"https://cdn.robovinci.xyz/imageframes/{n}.initial.json",
+                      to_filename=f"{problems_dir}/{n}.initial.json")
     else:
         print(f"File {n} exists")
 
 
 def open_as_np(n):
-    img=Image.open(f"problems/{n}.png")
-    a=np.asarray(img)
-    return a[::-1,:].swapaxes(0,1)
-    
-def save_from_np(path, np_array):
-    img = Image.fromarray(np_array.swapaxes(0, 1)[::-1,:])
-    img.save(path, "PNG")
+    img = Image.open(f"problems/{n}.png")
+    a = np.asarray(img)
+    return a[::-1, :].swapaxes(0, 1)
 
+
+def save_from_np(path, np_array):
+    img = Image.fromarray(np_array.swapaxes(0, 1)[::-1, :])
+    img.save(path, "PNG")
