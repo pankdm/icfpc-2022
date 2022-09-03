1837a5004a834e26eeb511c0674ba2c749b3db78
Mikhail Kalugin <mikhail.kalugin@gmail.com>
2022-09-03 00:00:35 -0700

    added caching to the solver, 1/4 split

--- solver/binar_solver.py
+++ solver/binar_solver.py
@@ -107,12 +107,19 @@ class Solver:
     def __init__(self, ref_img, max_depth=4):
         self.ref_img = ref_img
         self.max_depth = max_depth
+        self.cache = {}
+        self.avg_color_cache = {}
 
     def subimage(self, sp: Shape):
         return self.ref_img[sp.x1:sp.x2, sp.y1:sp.y2]
 
     def improve(self, block_id: str, sp: Shape, current_color, depth) -> Option:
         # print(f"improve {block_id} at {sp} depth {depth}")
+
+        cache_key = tuple([sp.x1, sp.x2, sp.y1, sp.y2] + list(current_color))
+        if cache_key in self.cache:
+            o = self.cache[cache_key]
+            return Option(cmds=[cmd.replace("$", block_id) for cmd in o.cmds], score=o.score)
 
         options = []
 
@@ -129,8 +136,13 @@ class Solver:
 
         # avg_color = [round(v) for v in subimg.mean(axis=0).mean(axis=0)]
         # start = time.time()
-        avg_color = [round(v) for v in gm.geometric_median(
-            subimg.reshape((subimg.shape[0] * subimg.shape[1], 4)), eps=0.5)]
+        avg_color_cache_key = (sp.x1, sp.x2, sp.y1, sp.y2)
+        if avg_color_cache_key in self.avg_color_cache:
+            avg_color = self.avg_color_cache[avg_color_cache_key]
+        else:
+            avg_color = [round(v) for v in gm.geometric_median(
+                subimg.reshape((subimg.shape[0] * subimg.shape[1], 4)), eps=0.5)]
+            self.avg_color_cache[avg_color_cache_key] = avg_color
         # end = time.time()
         # print(f"geometric_median took {end - start} for {sp.size}")
 
@@ -138,13 +150,13 @@ class Solver:
         if avg_color != current_color:
             new_similarity = costs.float_simil(subimg - avg_color)
             options.append(Option(
-                cmds=[f"color [{block_id}] {avg_color}"],
+                cmds=[f"color [$] {avg_color}"],
                 score=color_cost + new_similarity))
 
             if depth < self.max_depth:
                 o = self.improve(block_id=block_id, sp=sp, current_color=avg_color, depth=depth+1)
                 options.append(Option(
-                    cmds=[f"color [{block_id}] {avg_color}"] + o.cmds,
+                    cmds=[f"color [$] {avg_color}"] + o.cmds,
                     score=color_cost + o.score))
 
             best_similarity = min(current_similarity, new_similarity)
@@ -153,7 +165,7 @@ class Solver:
             def make_program(cmd, cmd_cost, subshapes):
                 ops = [
                     self.improve(
-                        sp=subshape, block_id=f"{block_id}.{i}", current_color=current_color, depth=depth+1)
+                        sp=subshape, block_id=f"$.{i}", current_color=current_color, depth=depth+1)
                     for i, subshape in enumerate(subshapes)
                 ]
                 subcmds = list(itertools.chain.from_iterable(
@@ -166,21 +178,21 @@ class Solver:
 
             lin_split_cost = costs.get_cost(costs.COSTS.LINECUT, sp.size)
             if best_similarity > color_cost + lin_split_cost:  # only try if we can recolor at least one of the new blocks
-                for ratio in [0.5]:
+                for ratio in [0.25, 0.5, 0.75]:
                     x_pos = round(sp.x1 + sp.w * ratio)
-                    options.append(make_program(cmd=f"cut [{block_id}] [x] [{x_pos}]",
+                    options.append(make_program(cmd=f"cut [$] [x] [{x_pos}]",
                                                 cmd_cost=lin_split_cost,
                                                 subshapes=sp.split_x(x_pos))
                                 )
                     y_pos = round(sp.y1 + sp.h * ratio)
-                    options.append(make_program(cmd=f"cut [{block_id}] [y] [{y_pos}]",
+                    options.append(make_program(cmd=f"cut [$] [y] [{y_pos}]",
                                                 cmd_cost=lin_split_cost,
                                                 subshapes=sp.split_y(y_pos))
                                 )
 
             pt_split_cost = costs.get_cost(costs.COSTS.POINTCUT, sp.size)
             if best_similarity > color_cost + lin_split_cost:  # only try if we can recolor at least one of the new blocks
-                options.append(make_program(cmd=f"cut [{block_id}] [{mid_x}, {mid_y}]",
+                options.append(make_program(cmd=f"cut [$] [{mid_x}, {mid_y}]",
                                             cmd_cost=pt_split_cost,
                                             subshapes=sp.split_xy(mid_x, mid_y))
                            )
@@ -194,20 +206,23 @@ class Solver:
                 best_option = o
         # if depth < 1: print(f"{block_id} best option = {best_option}")
 
-        return best_option
+        self.cache[cache_key] = best_option
+
+        return Option(cmds=[cmd.replace("$", block_id) for cmd in best_option.cmds],
+                      score=best_option.score)
 
 
 def main():
-    for i in range(1, 26):
+    for i in range(2, 26):
         img = open_as_np(i)
 
         print(f"# Solving {i}")
         solver = Solver(ref_img=img, max_depth=5)
-        o = solver.improve(0, Shape(0, 0, 400, 400), [255, 255, 255, 255], 0)
-        # print(f"Solution: {o.score}")
-        # print("\n".join(o.cmds))
+        o = solver.improve("0", Shape(0, 0, 400, 400), [255, 255, 255, 255], 0)
+        print(f"Solution: {o.score}")
+        print("\n".join(o.cmds))
 
-        with open(f"{sys.path[0]}/../solutions/binary_solver_2/{i}.txt", "wt") as f:
+        with open(f"{sys.path[0]}/../solutions/binary_solver_3/{i}.txt", "wt") as f:
             f.write("\n".join(o.cmds)+"\n")
 
     print(f"DONE")
