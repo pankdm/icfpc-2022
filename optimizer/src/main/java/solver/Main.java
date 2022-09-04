54ee0f9b477ea1277116fa371736e4d73459f980
Dmitry Korolev <dmitry.korolyov@gmail.com>
2022-09-04 15:22:54 -0700

    improve optimizer output

--- optimizer/src/main/java/solver/Main.java
+++ optimizer/src/main/java/solver/Main.java
@@ -112,12 +112,11 @@ public class Main {
                     currentProgram.addAll(program.subList(i+1, len));
 
                     ExecutionResult result = test(count, problemFileName, initialStateFile, currentProgram);
-                    //System.out.println("Score: " + result.totalCost);
                     count++;
 
                     if (result.totalCost < bestResult.totalCost) {
+                        System.out.println(String.format("[%d/%d] new best score found: %d -> %d", i, len, bestResult.totalCost, result.totalCost));
                         bestResult = result;
-                        System.out.println(String.format("[%d] new best score found: %d -> %d", count, result.totalCost, bestResult.totalCost));
                     }
                 }
             } if (command.getType() == MoveType.POINT_CUT) {
@@ -131,12 +130,11 @@ public class Main {
                         currentProgram.addAll(program.subList(i+1, len));
 
                         ExecutionResult result = test(count, problemFileName, initialStateFile, currentProgram);
-                        System.out.println("Score: " + result.totalCost);
                         count++;
 
                         if (result.totalCost < bestResult.totalCost) {
+                            System.out.println(String.format("[%d/%d] new best score found: %d -> %d", i, len, bestResult.totalCost, result.totalCost));
                             bestResult = result;
-                            System.out.println(String.format("[%d] new best score found: %d -> %d", count, result.totalCost, bestResult.totalCost));
                         }
                     }
                 }
