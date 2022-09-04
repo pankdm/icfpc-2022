f7876ae744ba4b60890b3184c52587546a7a1db4
Dmitry Panin <pankdm@gmail.com>
2022-09-04 11:09:48 -0700

    Download solutions

--- server/api/icfpc.py
+++ server/api/icfpc.py
@@ -6,6 +6,11 @@ import os
 import requests
 import requests.auth
 
+from time import time 
+import os
+from collections import defaultdict
+
+
 API_ROOT = 'https://robovinci.xyz/api'
 ICFPC_USER_EMAIL = os.environ.get('ICFPC_USER_EMAIL')
 ICFPC_USER_PASSWORD = os.environ.get('ICFPC_USER_PASSWORD')
@@ -86,3 +91,43 @@ def proxy_get_endpoint(path):
 def check_auth():
     ensure_auth()
     return { 'user': icfpc_client.email, 'expires': icfpc_client.expires.isoformat() }
+
+
+def get_submission(id):
+    ensure_auth()
+    response = icfpc_client.session.get(API_ROOT+f'/submissions/{id}')
+    response.raise_for_status()
+    return response.json()
+
+
+def download_best_submissions():
+    ensure_auth()
+
+    ts = int(time())
+    folder = os.path.dirname(__file__)+f'/../../best_solutions/{ts}'
+    os.mkdir(folder)
+
+    res = get_submissions()
+    # print(res)
+
+    all_problems = defaultdict(list)
+    for submit_info in res['submissions']:
+        problem_id = submit_info['problem_id']
+        if 'score' not in submit_info:
+            continue
+        score = submit_info['score']
+        id = submit_info['id']
+        all_problems[problem_id].append((score, id))
+
+    # print (all_problems)
+    
+    for problem_id, scores in all_problems.items():
+        (score, id) = min(scores)
+        detailed_info = get_submission(id)
+        # print (problem_id, score, id, detailed_info)
+        url = detailed_info['file_url']
+        code_response = requests.get(url)
+        print (f'Downloading {id} as {problem_id}_{score}.txt')
+        with open(f"{folder}/{problem_id}_{score}.txt", "wt") as f:
+            f.write(str(code_response.content))
+    
